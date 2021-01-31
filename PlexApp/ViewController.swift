b2d4ecbb72e4266a253cd33cb957cfe43c7022b4
HishD <hish2k15@gmail.com>
2021-01-31 14:31:56 +0530

    Added auto-layout constraints

--- PlexApp/ViewController.swift
+++ PlexApp/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerContinueWithApple: UIView!
+    @IBOutlet weak var containerContinueWithFB: UIView!
+    @IBOutlet weak var containerContinueWithGoogle: UIView!
+    @IBOutlet weak var containerContinueWithMail: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        containerContinueWithApple.layer.cornerRadius = 10
+        containerContinueWithFB.layer.cornerRadius = 10
+        containerContinueWithGoogle.layer.cornerRadius = 10
+        containerContinueWithMail.layer.cornerRadius = 10
     }
 
 
